3d1f6b48ffb54cf9fbd80ece03d0f6a29cdc61e1
Ankit Bhargava <ankit@constructivIQ.com>
2024-05-16 19:41:48 +0530

    Back-button.

--- app/pages/layout.tsx
+++ app/pages/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./style.css";
+import Image from "next/image";
+import { prefix } from "../prefix";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,5 +12,21 @@ export default function PageNumberLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="">{children}</div>;
+  return (
+    <div className="">
+      <div className="w-full p-5 flex">
+        <Link href="/">
+          <Image
+            alt=""
+            src={`${prefix}/icons/back-icon.png`}
+            width={30}
+            height={30}
+            className=""
+            unoptimized
+          />
+        </Link>
+      </div>
+      {children}
+    </div>
+  );
 }
